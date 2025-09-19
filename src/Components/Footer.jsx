b3a9df36ca02224de8e35944f0a662bd98c55cda
Aastha <sharma.aastha1456@gmail.com>
2025-09-19 17:00:15 +0545

    UI- Carousel

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,20 +8,20 @@ const Footer = () => {
     <div className="flex flex-col items-center justify-center">
         <hr className="w-2/3 h-[2px] bg-[var(--line-sec)] border-0 mb-6" />
         <div className="flex mt-4">
-            <ul class="inter text-lg flex items-center text-[var(--footer)] font-medium justify-center">
-              <li class="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
+            <ul className="inter text-lg flex items-center text-[var(--footer)] font-medium justify-center">
+              <li className="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
                 Features
               </li>
-              <li class="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
+              <li className="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
                 Learn
               </li>
-              <li class="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
+              <li className="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
                 Pricing
               </li>
-              <li class="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
+              <li className="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
                 About
               </li>
-              <li class="relative px-4">
+              <li className="relative px-4">
                 Contact
               </li>
             </ul>
@@ -39,11 +39,11 @@ const Footer = () => {
           <p className="inter font-medium text-lg text-[var(--footer)] p-1">&copy; 2025 Investynn. All rights reserved.</p>
         </div>
         <div className="mb-20">
-         <ul class="flex items-center text-[var(--footer)] font-medium justify-center">
-          <li class="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
+         <ul className="flex items-center text-[var(--footer)] font-medium justify-center">
+          <li className="relative px-4 after:content-[''] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:h-5 after:w-[1px] after:bg-[var(--footer)] after:shadow-[0_0_2px_rgba(0,0,0,0.3)]">
             Terms
           </li>
-          <li class="relative px-4">
+          <li className="relative px-4">
             Privacy
           </li>
         </ul>
--- src/Components/Sec4.jsx
+++ src/Components/Sec4.jsx
@@ -1,70 +1,92 @@
-import InvestIllustration from '../assets/Invest_Illustration.png';
+import card1 from '../assets/cards/card1.png';
+import First_Strip from '../assets/cards/First_Strip.png';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Sec4 = () => {
+  // const settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1280,
+  //       settings: {
+  //         slidesToShow: 3,
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 900,
+  //       settings: {
+  //         slidesToShow: 2,
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 1,
+  //       }
+  //     }
+  //   ]
+  // };
   const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
-  };
+  centerMode: true,
+  centerPadding: "40px", // space around center card
+  slidesToShow: 3,
+  infinite: true,
+  // autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 500,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: { slidesToShow: 2, centerPadding: "30px" },
+    },
+    {
+      breakpoint: 900,
+      settings: { slidesToShow: 1, centerPadding: "20px" },
+    },
+  ],
+};
+
 
   const cards = [
     {
-      img: InvestIllustration,
-      title: "What is Investynn?",
-      heading: "Learn the basics",
-      desc: "Investynn makes investing simple, transparent, and accessible for everyone.",
+      img_f: card1,
+      img_s: First_Strip,
+      title: "Smart Tips",
+      heading: "Avoid Common Mistakes",
+      desc: "Description: Discover the do's and don'ts of trading so you can make smarter decisions and minimize risks.",
     },
     {
-      img: InvestIllustration,
-      title: "Why choose us?",
-      heading: "Our advantages",
-      desc: "We provide expert insights and easy-to-use tools for smarter investing.",
+      img_f: card1,
+      img_s: First_Strip,
+      title: "Smart Tips",
+      heading: "Avoid Common Mistakes",
+      desc: "Description: Discover the do's and don'ts of trading so you can make smarter decisions and minimize risks.",
     },
     {
-      img: InvestIllustration,
-      title: "How to start?",
-      heading: "Begin your journey",
-      desc: "Sign up, explore opportunities, and begin your investment journey today.",
+      img_f: card1,
+      img_s: First_Strip,
+      title: "Smart Tips",
+      heading: "Avoid Common Mistakes",
+      desc: "Description: Discover the do's and don'ts of trading so you can make smarter decisions and minimize risks.",
     },
     {
-      img: InvestIllustration,
-      title: "Secure & Reliable",
-      heading: "Safety first",
-      desc: "Your data and investments are safe with industry-leading security measures.",
+      img_f: card1,
+      img_s: First_Strip,
+      title: "Smart Tips",
+      heading: "Avoid Common Mistakes",
+      desc: "Description: Discover the do's and don'ts of trading so you can make smarter decisions and minimize risks.",
     },
-    {
-      img: InvestIllustration,
-      title: "Accessible to All",
-      heading: "For everyone",
-      desc: "Our platform is built for beginners and experts alike.",
-    },
+    
   ];
 
   return (
@@ -72,33 +94,76 @@ const Sec4 = () => {
       <h1 className="poppins text-6xl font-extrabold text-[var(--sec4-title)] text-center p-6">
         Getting Started Guide
       </h1>
+      <div className="absolute inset-0 flex flex-wrap justify-center items-center opacity-10 pointer-events-none">
+    {Array.from({ length: 40 }).map((_, i) => (
+      <span
+        key={i}
+        className="poppins text-[5rem] font-extrabold text-[#d9e6ff] m-4 select-none"
+      >
+        BLOG
+      </span>
+    ))}
+  </div>
 
-      <Slider {...settings} className="mt-8 py-4 mb-4">
-        {cards.map((c, i) => (
-          <div key={i} className="p-4">
-            <div
-              className={`flex flex-col shadow-lg rounded-xl h-[28rem] w-3xs transform transition-all duration-300 ${
-                i % 2 === 1 ? "translate-y-8" : "translate-y-0"
-              }`}
-            >
-              <div className="flex flex-col rounded-xl w-3xs h-full bg-white">
-                <div className="rounded-t-xl overflow-hidden flex-shrink-0 h-56 flex items-center justify-center">
-                  <img src={c.img} alt="Card Visual" className="w-full h-full object-contain" />
-                </div>
-                <div className="px-2 mb-2 flex flex-col flex-1">
-                  <p className="text-[var(--sec4-gray)] font-semibold text-xl mt-4">
-                    {c.title}
-                  </p>
-                  <p className="text-base font-semibold text-[var(--sec4-gray-sec)]">
+      {/* <div className="relative w-[313px] shadow-2xl rounded-4xl overflow-hidden bg-white">
+        <img
+          src={First_Strip}
+          alt="Decoration"
+          className="absolute top-0 left-0 h-full z-0"
+        />
+
+        <img
+          src={card1}
+          alt="Common Mistakes"
+          className="w-full h-[200px] object-cover relative z-10 rounded-t-4xl"
+        />
+
+        <div className="px-6 py-6 bg-white relative z-10 rounded-tl-4xl">
+          <h3 className="poppins font-extrabold text-xl text-[#838383]">Smart Tips</h3>
+          <p className="poppins text-lg font-semibold text-[#999999] mt-2 leading-[113%]">
+            Avoid Common Mistakes
+          </p>
+          <p className="poppins text-[#999999] mr-4 leading-tight">
+            Description: Discover the do’s and don’ts of trading so you can make
+            smarter decisions and minimize risks.
+          </p>
+        </div>
+      </div> */}
+      
+      <div className="w-full py-2">
+
+        <Slider {...settings} className="py-10 h-auto">
+          {cards.map((c, i) => (
+            <div key={i} className="px-3"> 
+              <div
+                className={`relative w-[313px] mx-auto shadow-2xl rounded-4xl overflow-hidden bg-white transform transition-all duration-300 ${
+                  i % 2 === 1 ? "translate-y-8" : "translate-y-0"
+                }`}
+              >
+                <img
+                  src={c.img_s}
+                  alt="Decoration"
+                  className="absolute top-0 left-0 h-full z-0"
+                />
+                <img
+                  src={c.img_f}
+                  alt={c.title}
+                  className="w-full h-[200px] object-cover relative z-10 rounded-t-4xl"
+                />
+                <div className="px-6 py-6 bg-white relative z-10 rounded-tl-4xl">
+                  <h3 className="poppins font-extrabold text-xl text-[#838383]">{c.title}</h3>
+                  <p className="poppins text-lg font-semibold text-[#999999] mt-2 leading-[113%]">
                     {c.heading}
                   </p>
-                  <p className="text-[var(--sec4-gray-sec)] mt-2">{c.desc}</p>
+                  <p className="poppins text-[#999999] mr-4 leading-tight">{c.desc}</p>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      </div>
+      
+
     </section>
   );
 };
